dc975b6315cc5b3c9b52ad841f21dbd023d0fa74
kushagradixit0312 <106438250+kushagradixit0312@users.noreply.github.com>
2022-05-29 18:46:20 +0530

    Update calculator.py

--- funtion/calculator.py
+++ funtion/calculator.py
@@ -11,3 +11,15 @@ print ("a. Add")
 print ("b. Subtract")    
 print ("c. Multiply")    
 print ("d. Divide")
+f choice == 'a':    
+   print (num_1, " + ", num_2, " = ", add(num_1, num_2))    
+    
+elif choice == 'b':    
+   print (num_1, " - ", num_2, " = ", subtract(num_1, num_2))    
+    
+elif choice == 'c':    
+   print (num1, " * ", num2, " = ", multiply(num1, num2))    
+elif choice == 'd':    
+   print (num_1, " / ", num_2, " = ", divide(num_1, num_2))    
+else:    
+   print ("This is an invalid input")    
